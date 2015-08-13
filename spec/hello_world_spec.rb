747fdbcce426ecb74c090c074a2bf15d924529ef
Seiji Naganuma <snaganumas@gmail.com>
2015-08-13 08:11:10 -0400

    add another spec

--- spec/hello_world_spec.rb
+++ spec/hello_world_spec.rb
@@ -1,4 +1,4 @@
-describe "Hello World" do 
+describe "Hello World" do
 
   it 'puts "Hello World!"' do
     # $stdout represents the output of your program, don't worry about how
@@ -7,4 +7,10 @@ describe "Hello World" do
     load './hello_world.rb'
   end
 
+  it 'puts "goodbye world!"' do
+    # $stdout represents the output of your program, don't worry about how
+    # we are testing this, focus on simply coding your solution.
+    expect($stdout).to receive(:puts).with("goodbye world!")
+    load './goodbye_world.rb'
+  end
 end
